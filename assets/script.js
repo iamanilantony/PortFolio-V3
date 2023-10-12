b31fc230c8d2a755bde0ffd4ac1eb2b0e68559ba
Anil Antony <anilantony3134@gmail.com>
2023-10-12 13:34:16 +0530

    fix: added links

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,6 @@ function getDurationBetweenDates(startDate) {
   const start = new Date(new Date(startDate).setHours(0, 0, 0, 0));
   const end = new Date();
 
-  console.log(start, end);
-
   const timeDifference = end - start;
 
   // Calculate the number of milliseconds in a year, month, and week
@@ -23,8 +21,6 @@ function getDurationBetweenDates(startDate) {
     timeDifference % millisecondsInWeek / (24 * 60 * 60 * 1000)
   );
 
-  console.log(timeDifference, "time");
-
   return `${years} ${years > 1 ? "years" : "year"} ${months} ${months > 1
     ? "months"
     : "month"} ${weeks} ${weeks > 1 ? "weeks" : "week"} ${days} ${days > 1
@@ -38,45 +34,45 @@ const result = getDurationBetweenDates("20 May 2022");
 
 date.innerHTML = result;
 
-var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+// var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+// var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-// Change the icons inside the button based on previous settings
-if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  themeToggleLightIcon.classList.remove("hidden");
-} else {
-  themeToggleDarkIcon.classList.remove("hidden");
-}
+// // Change the icons inside the button based on previous settings
+// if (
+//   localStorage.getItem("color-theme") === "dark" ||
+//   (!("color-theme" in localStorage) &&
+//     window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+//   themeToggleLightIcon.classList.remove("hidden");
+// } else {
+//   themeToggleDarkIcon.classList.remove("hidden");
+// }
 
-var themeToggleBtn = document.getElementById("theme-toggle");
+// var themeToggleBtn = document.getElementById("theme-toggle");
 
-themeToggleBtn.addEventListener("click", function() {
-  // toggle icons inside button
-  themeToggleDarkIcon.classList.toggle("hidden");
-  themeToggleLightIcon.classList.toggle("hidden");
+// themeToggleBtn.addEventListener("click", function() {
+//   // toggle icons inside button
+//   themeToggleDarkIcon.classList.toggle("hidden");
+//   themeToggleLightIcon.classList.toggle("hidden");
 
-  // if set via local storage previously
-  if (localStorage.getItem("color-theme")) {
-    if (localStorage.getItem("color-theme") === "light") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    }
+//   // if set via local storage previously
+//   if (localStorage.getItem("color-theme")) {
+//     if (localStorage.getItem("color-theme") === "light") {
+//       document.documentElement.classList.add("dark");
+//       localStorage.setItem("color-theme", "dark");
+//     } else {
+//       document.documentElement.classList.remove("dark");
+//       localStorage.setItem("color-theme", "light");
+//     }
 
-    // if NOT set via local storage previously
-  } else {
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    }
-  }
-});
+//     // if NOT set via local storage previously
+//   } else {
+//     if (document.documentElement.classList.contains("dark")) {
+//       document.documentElement.classList.remove("dark");
+//       localStorage.setItem("color-theme", "light");
+//     } else {
+//       document.documentElement.classList.add("dark");
+//       localStorage.setItem("color-theme", "dark");
+//     }
+//   }
+// });
